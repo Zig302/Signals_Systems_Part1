d4526696d31b15fd7a791195e5eac17d2ac23b7f
Alex <alex20079xx@gmail.com>
2024-08-05 16:04:37 +0300

    Made a progress

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ def Exercise8(flag=True):
     if flag:
         # Plot the convoluted signal
         plt.figure(figsize=(10, 6))
-        plt.stem(n, g_k, basefmt=" ")
+        plt.stem(n, np.real(g_k), basefmt=" ")
         plt.title('Convoluted Signal $g(k)$')
         plt.xlabel('$k$')
         plt.ylabel('$g(k)$')
@@ -221,6 +221,15 @@ def Exercise9():
     # Signal h(n)
     h_n = a_n * np.sin(2 * np.pi * 500 * n / N)
 
+    # Plot the original h_n signal
+    plt.figure(figsize=(10, 6))
+    plt.stem(n, h_n, basefmt=" ")
+    plt.title('Original Signal $h(n)$')
+    plt.xlabel('$n$')
+    plt.ylabel('$h(n)$')
+    plt.grid(True)
+    plt.show()
+
     # Transfer h_n -> h_k
     k = np.arange(-1000, 1001)
     h_k = np.zeros(N, dtype=complex)
@@ -229,28 +238,46 @@ def Exercise9():
             h_k[ki + 1000] += h_n[x + 1000] * np.exp(-1j * ki * 2 * np.pi * x / N)
         h_k[ki + 1000] *= (1 / N)
 
+    # Plot the transformed h_k signal
+    plt.figure(figsize=(10, 6))
+    plt.stem(n, h_k, basefmt=" ")
+    plt.title('Transformed Signal $h(k)$')
+    plt.xlabel('$k$')
+    plt.ylabel('$h(k)$')
+    plt.grid(True)
+    plt.show()
+
     # Let's bring g_k from exe8
     g_k = Exercise8(False)
 
     # calculate H_k
     H_k = np.zeros_like(k, dtype=complex)
     H_k = -1j * np.sign(k)
-    H_k[np.abs(k) >= 1000] = 0
+    H_k[1000] = 0
 
     # calculate h_kk = H_k * g_k
     h_kk = H_k * g_k
+
+    # Plot the Hilbert signal
+    plt.figure(figsize=(10, 6))
+    plt.stem(k, np.imag(h_kk), basefmt=" ")
+    plt.title('After Hilbert Signal $h(k)$')
+    plt.xlabel('$k$')
+    plt.ylabel('$h(k)$')
+    plt.grid(True)
+    plt.show()
 
     # now calculate h_nn which is inverse transform of h_kk
     h_nn = np.zeros_like(n, dtype=complex)
     for ni in range(len(n)):
         h_nn[ni] = np.sum(h_kk * np.exp(1j * 2 * np.pi * k * n[ni] / N))
 
-    # Plot the convoluted signal
+    # Plot the Transformed back Hilbert signal
     plt.figure(figsize=(10, 6))
     plt.stem(n, h_nn, basefmt=" ")
-    plt.title('Convoluted Signal $h(k)$')
-    plt.xlabel('$k$')
-    plt.ylabel('$h(k)$')
+    plt.title('After Hilbert Signal in time $h(n)$')
+    plt.xlabel('$n$')
+    plt.ylabel('$h(n)$')
     plt.grid(True)
     plt.show()
 
@@ -318,14 +345,121 @@ def Exercise11():
     plt.show()
 
 
-def Exercise12():
-    # TODO
-    pass
+def Exercise12(D=3):
+    w = np.linspace(-np.pi, np.pi, 2001)  # We sampled it 2*pi/N times
+    N = 2001  # Number of samples
+    n = np.arange(-1000, 1001)
+
+    # Signal a(n)
+    a_n = np.where(np.abs(n) < 100, 1, 0)
+
+    # It's the formula for DTFT, but we actually do a DFT
+    # when we sample on a single period of DTFT between -pi to pi
+    X_1 = np.zeros_like(w, dtype=complex)
+    for k in range(N):
+        X_1 += a_n[k] * np.exp(-1j * w * k)
+
+    # Plot X(jw)
+    plt.figure(figsize=(10, 6))
+    plt.stem(w, X_1, basefmt=" ")
+    plt.title('DFT of $a(n)$')
+    plt.xlabel('$k$')
+    plt.ylabel('$X(k)$')
+    plt.grid(True)
+    plt.show()
+
+    # Decimation by a factor of D that we chose and DFT after it
+    a_dec_2 = np.zeros(N)
+    a_dec_2[::D] = a_n[::D]  # Sample every D indexes
+
+    X_2 = np.zeros(N, dtype=complex)
+    for k2 in range(N):
+        X_2 += a_dec_2[k2] * np.exp(-1j * w * k2)
+
+    plt.figure(figsize=(10, 6))
+    plt.stem(w, abs(X_2), basefmt=" ")
+    plt.title('DFT of $a_2(n)$')
+    plt.xlabel('$k$')
+    plt.ylabel('$X(k)$')
+    plt.grid(True)
+    plt.show()
 
 
-def Exercise13():
-    # TODO
-    pass
+def Exercise13(padding=2, flag=True):
+    N = 2001  # Number of samples
+    n = np.arange(-1000, 1001)
+
+    # Signal a(n)
+    a_n = np.where(np.abs(n) < 100, 1, 0)
+    a_n_padded = []
+    for coeff in range(N):
+        a_n_padded.append(a_n[coeff])  # Add the a_k coefficient
+        if coeff != N - 1:
+            a_n_padded.extend([0] * padding)  # Add the padding zeros
+
+    a_n_padded = np.array(a_n_padded)
+    N2 = len(a_n_padded)
+    w = np.linspace(-np.pi, np.pi, N2)
+    X_pad = np.zeros(N2, dtype=complex)
+    for k in range(N2):
+        X_pad += a_n_padded[k] * np.exp(-1j * w * k)
+
+    if flag:
+        # Plot X_2(k)
+        plt.figure(figsize=(10, 6))
+        plt.stem(w, X_pad)
+        plt.title('DFT of $X_M(K)$ with Zero Padding')
+        plt.xlabel('Frequency: $K$')
+        plt.ylabel('$X_M(K)$')
+        plt.grid(True)
+        plt.show()
+    return N2, X_pad, w
+
+
+def Exercise14(padding=2, flag=True):
+    # We receive the interpolated samples in frequency from exe13
+    N2, X_2, w_2 = Exercise13(padding, False)
+
+    # Now build the filters
+    cutoff_freq = np.pi / padding
+    # Create the filter in frequency domain
+    H_2 = np.where(np.abs(w_2) <= cutoff_freq, 1, 0)
+    X_2_filtered = X_2 * H_2
+
+    # Calculate filter in time
+    N = len(H_2)
+    h_n = np.zeros(N, dtype=complex)
+    n_values = np.arange(-(N - 1) / 2, (N - 1) / 2 + 1)
+    for n_idx, n in enumerate(n_values):
+        h_n[n_idx] = np.sum(H_2 * np.exp(1j * w_2 * n)) / N
+
+    if flag:
+        # Plot the filter
+        plt.figure(figsize=(10, 6))
+        plt.stem(n_values, h_n)
+        plt.title('$H_M(K)$')
+        plt.xlabel('Frequency: $K$')
+        plt.ylabel('$H_M(K)$')
+        plt.grid(True)
+        plt.show()
+
+    # Inverse DFT
+    N = len(X_2_filtered)
+    x_n = np.zeros(N, dtype=complex)
+    for n in range(N):
+        x_n[n] = np.sum(X_2_filtered * np.exp(1j * w_2 * n)) / N
+
+    if flag:
+        # Plot X_2(k)
+        plt.figure(figsize=(10, 6))
+        plt.plot(n_values, x_n)
+        plt.title('DFT of $X_M(K)$ with Zero Padding')
+        plt.xlabel('Frequency: $K$')
+        plt.ylabel('$X_M(K)$')
+        plt.grid(True)
+        plt.show()
+
+    return H_2, h_n, x_n
 
 
 def Exercise15():
@@ -353,10 +487,10 @@ if __name__ == '__main__':
     # Exercise8()
     # Exercise9()
     # Exercise10()
-    Exercise11()
+    # Exercise11()
     # Exercise12()
     # Exercise13()
-    # Exercise14()
+    Exercise14()
     # Exercise15()
     # Exercise16()
     # Exercise17()
